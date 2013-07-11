091071d566ba503ff55d80c840822e04111be724
Tomasz Janczuk <tomasz@janczuk.org>
2013-07-11 10:05:51 +0200

    fix test

--- checkplatform.js
+++
@@ -1,5 +0,0 @@
-if (process.platform !== 'win32') {
-	console.error('******************************************************');
-	console.error('ERROR: The httpsys module is only supported on Windows');
-	console.error('******************************************************');
-}
--- test/102_hello.js
+++ test/102_hello.js
@@ -1,32 +1,40 @@
 var http = require('../lib/httpsys.js').http()
-	, assert = require('assert');
+    , assert = require('assert');
 
 var port = process.env.PORT || 3000;
+var server;
 
 describe('hello, world', function () {
 
-	it('works', function (done) {
-		var server = http.createServer(function (req, res) {
-			res.writeHead(200, { 'Content-Type': 'text/plain' });
-			res.end('Hello, world!');
-		});
+    afterEach(function (done) {
+        if (server) {
+            server.close();
+            server = undefined;
+        }
 
-		server.listen(port);
+        done();
+    });
 
-		var request = http.get('http://localhost:' + port, function (res) {
-			assert.equal(res.statusCode, 200);
-			assert.equal(res.headers['content-type'], 'text/plain');
-			var body = '';
-			res.on('data', function (chunk) { body += chunk; });
-			res.on('end', function () {
-				assert.equal(body, 'Hello, world!');
-				done();
-			})
-		});
+    it('works', function (done) {
+        server = http.createServer(function (req, res) {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Hello, world!');
+        });
 
-		request.on('error', function (e) {
-			assert.ifError(e);
-		});
-	});
+        server.listen(port);
+
+        var request = http.get('http://localhost:' + port, function (res) {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.headers['content-type'], 'text/plain');
+            var body = '';
+            res.on('data', function (chunk) { body += chunk; });
+            res.on('end', function () {
+                assert.equal(body, 'Hello, world!');
+                done();
+            })
+        });
+
+        request.on('error', assert.ifError);
+    });
 
 });
